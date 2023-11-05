93aead0e1ef6a2fe8d221b0f95d4070385dfb6e0
Yunyang <sunnycloudyang@outlook.com>
2023-11-06 00:25:55 +0800

    Update: update catalog behavior

--- docs/js/catalog.js
+++ docs/js/catalog.js
@@ -1,8 +1,6 @@
 import { RenderCatalog } from "./renderCatalog.js";
 
 function getCatalog() {
-    const uid = localStorage.getItem('id');
-    const token = localStorage.getItem('token');
     $.ajax({
         url: 'http://54.79.139.73:80/v1/catalog/' + uid,
         type: 'GET',
@@ -12,6 +10,10 @@ function getCatalog() {
         },
         success: function (res) {
             // console.log(res.data.list);
+            if (res.data.list.length == 0) {
+                $('#content').html('<h3 class="text-center">Catalog is empty</h3>');
+                return;
+            }
             RenderCatalog(res.data.list);
         },
         error: function (error) {
@@ -20,4 +22,12 @@ function getCatalog() {
     });
 }
 
-getCatalog();
+const uid = localStorage.getItem('id');
+const token = localStorage.getItem('token');
+
+if (!token) {
+    $('#content').html('<h3 class="text-center">Please login to view catalog</h3>');
+}
+else {
+    getCatalog();
+}
